2c0234ac820c27ebc399d16b53b1e65c0a450315
Sardorbek Khujaev <58874777+iamsardorbek@users.noreply.github.com>
2020-04-29 19:43:38 +0500

    Login backend completed (unverified)

--- app/src/main/java/com/akfa/apsproject/Login.java
+++ app/src/main/java/com/akfa/apsproject/Login.java
@@ -6,11 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,80 +18,71 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-
 public class Login extends AppCompatActivity {
     private TextView loading, tip;
     private ImageView titlePic;
-    private EditText accessCode;
-    private Button checkThisPoint;
-    private Spinner spinnerPults;
-    private String correctAccessCode = "akfa";
+    private EditText passwordView, loginView;
+    private Button enter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initInstances();
-        checkThisPoint.setOnClickListener(new View.OnClickListener() {
+        enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(accessCode.getText().toString().equals(correctAccessCode)
-                        && !spinnerPults.getSelectedItem().toString().equals(getString(R.string.spinner_pults_prompt)))
-                {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.putExtra("Номер пункта", spinnerPults.getSelectedItem().toString());
-                    startActivity(intent);
-                }
-                else
-                    Toast.makeText(getApplicationContext(), "Неверный код доступа или не выбран пульт", Toast.LENGTH_LONG).show();
+                String login = loginView.getText().toString();
+                final String password = passwordView.getText().toString();
+                loading.setVisibility(View.VISIBLE);
+                DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users/" + login);
+                dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot user) {
+                        loading.setVisibility(View.INVISIBLE);
+                        if(user.exists())
+                        {//3 ifs with boolean checker functions in condition: isOperator, isMaster, isRepairer
+                            if(user.child("password").getValue().toString().equals(password)) {
+                                if (user.child("position").getValue().toString().equals("operator")) {
+                                    Intent openPult = new Intent(getApplicationContext(), MainActivity.class);
+                                    openPult.putExtra("Номер пульта", user.child("pultNo").getValue().toString());
+                                    startActivity(openPult);
+                                } else if (user.child("position").getValue().toString().equals("master")) {
+                                    Intent openPult = new Intent(getApplicationContext(), QuestMainActivity.class);
+                                    openPult.putExtra("Номер пульта", user.child("pultNo").getValue().toString());
+                                    startActivity(openPult);
+                                } else if (user.child("position").getValue().toString().equals("repairer")) {
+                                    Intent openPult = new Intent(getApplicationContext(), RepairersProblemsList.class);
+                                    startActivity(openPult);
+                                }
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(), "Неверный пароль", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Такого пользователя не существует\nВнимательно заполните поля", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
             }
         });
     }
+
     private void initInstances()
     {
-        spinnerPults = findViewById(R.id.spinner_pults);
-        accessCode = findViewById(R.id.access_code);
-        checkThisPoint = findViewById(R.id.check_this_point);
+        loginView = findViewById(R.id.access_login);
+        passwordView = findViewById(R.id.password);
+        enter = findViewById(R.id.enter);
         loading = findViewById(R.id.loading);
         titlePic = findViewById(R.id.title_pic);
         tip = findViewById(R.id.tip);
-        //невидимы пока с базы не загрузил данные
-        titlePic.setVisibility(View.INVISIBLE);
-        tip.setVisibility(View.INVISIBLE);
-        spinnerPults.setVisibility(View.INVISIBLE);
-        accessCode.setVisibility(View.INVISIBLE);
-        checkThisPoint.setVisibility(View.INVISIBLE);
-        setSpinnerPults();
-    }
-
-    private void setSpinnerPults()
-    {
-
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
-        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot wholeDB) {
-                ArrayList<String> pultsList = new ArrayList<String>();
-                pultsList.add(getString(R.string.spinner_pults_prompt));
-                for(DataSnapshot pult : wholeDB.getChildren())
-                {
-                    pultsList.add(pult.getKey());
-                }
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.spinner_item, pultsList);
-                arrayAdapter.setDropDownViewResource(R.layout.spinner_item);
-                spinnerPults.setAdapter(arrayAdapter);
-                titlePic.setVisibility(View.VISIBLE);
-                tip.setVisibility(View.VISIBLE);
-                spinnerPults.setVisibility(View.VISIBLE);
-                accessCode.setVisibility(View.VISIBLE);
-                checkThisPoint.setVisibility(View.VISIBLE);
-                loading.setVisibility(View.INVISIBLE);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
     }
 }
